e0a0d574d018679f053eebc27eb27bec11843e90
wangyi <godspeed_china@yeah.net>
2024-06-09 14:00:10 +0800

    init

--- CleanChar.cpp
+++ CleanChar.cpp
@@ -5,7 +5,7 @@ int	main(void){
 	int	c;
 	while((c=fgetc(stdin))!=EOF){
 		char	b=c;
-		if(b<0)	fputc(b,stdout);
+		if(b<0||(b>='0'&&b<='9')||(b>='a'&&b<='z')||(b>='A'&&b<='Z'))	fputc(b,stdout);
 	}
 	return	0;
 }
